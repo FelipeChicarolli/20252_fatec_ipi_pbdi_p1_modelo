4c3d73bc4457d02b1ea189e0dbc8aa225b341d54
Felipe Chicarolli <felipe.chicarolli.estudar@gmail.com>
2025-10-07 11:12:23 -0300

    prova(02): realizado exercicio 1

--- script.sql
+++ script.sql
@@ -1,6 +1,21 @@
 -- ----------------------------------------------------------------
 -- 1 Base de dados e criação de tabela
---escreva a sua solução aqui
+-- CREATE TABLE provap1 (
+-- cod_vinho SERIAL PRIMARY KEY,
+-- country VARCHAR(200),
+-- description VARCHAR (2000),
+-- designation VARCHAR (200),
+-- points INT,
+-- price FLOAT,
+-- province VARCHAR(200),
+-- region_1 VARCHAR(200),
+-- region_2 VARCHAR(200),
+-- taster_name VARCHAR(200),
+-- taster_twitter_handle VARCHAR(200),
+-- title VARCHAR(200),
+-- variety VARCHAR(200),
+-- winery 
+-- );
 
 
 -- ----------------------------------------------------------------
