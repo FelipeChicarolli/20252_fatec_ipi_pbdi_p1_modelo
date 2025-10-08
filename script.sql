b38d5742f8c673041df940725420e61b65f72ee9
Felipe Chicarolli <felipe.chicarolli.estudar@gmail.com>
2025-10-08 19:18:54 -0300

    prova (07): consertado o exercicio 1

--- script.sql
+++ script.sql
@@ -1,22 +1,12 @@
 -- ----------------------------------------------------------------
 -- 1 Base de dados e criação de tabela
--- CREATE TABLE provap1 (
--- cod_vinho SERIAL PRIMARY KEY,
--- country VARCHAR(200),
--- description VARCHAR (2000),
--- designation VARCHAR (200),
--- points INT,
--- price FLOAT,
--- province VARCHAR(200),
--- region_1 VARCHAR(200),
--- region_2 VARCHAR(200),
--- taster_name VARCHAR(200),
--- taster_twitter_handle VARCHAR(200),
--- title VARCHAR(200),
--- variety VARCHAR(200),
--- winery 
--- );
-
+CREATE TABLE provap1 (
+cod_vinho SERIAL PRIMARY KEY,
+country VARCHAR(200),
+description VARCHAR (2000),
+points INT,
+price FLOAT
+);
 
 -- ----------------------------------------------------------------
 -- 2 Cursor não vinculado (cálculo de preço médio)
