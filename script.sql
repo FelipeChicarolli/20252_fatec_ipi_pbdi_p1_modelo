efedd73daaf6c0118cfcf946c955d3b198fb6902
Felipe Chicarolli <felipe.chicarolli.estudar@gmail.com>
2025-10-12 12:41:00 -0300

    prova(11): modificado exercicios 2 e 3

--- script.sql
+++ script.sql
@@ -16,12 +16,15 @@ DECLARE
     v_pais VARCHAR(100);
     v_preco_medio NUMERIC(10, 2);
 BEGIN
-    OPEN cur_paises FOR SELECT DISTINCT country FROM vinhos;
+    OPEN cur_paises FOR SELECT DISTINCT country FROM provap1;
     LOOP
         FETCH cur_paises INTO v_pais; 
         EXIT WHEN NOT FOUND; 
-        SELECT AVG(price) INTO v_preco_medio FROM vinhos WHERE country = v_pais;
+        SELECT AVG(price) INTO v_preco_medio FROM provap1 WHERE country = v_pais;
         INSERT INTO analise_vinhos (nome_pais, preco_medio) VALUES (v_pais, v_preco_medio);
+
+        --RAISE NOTICE 'País: %, Preço Médio: %', v_pais, round(v_preco_medio, 2);
+
     END LOOP;
     CLOSE cur_paises;
 END $$;
@@ -33,22 +36,47 @@ DO $$
 DECLARE
     cur_descricao CURSOR (p_pais VARCHAR) FOR 
         SELECT description 
-        FROM vinhos 
+        FROM provap1
         WHERE country = p_pais 
         ORDER BY LENGTH(description) DESC 
         LIMIT 1;
+        
     v_registro_analise RECORD;
     v_descricao_longa TEXT;
 BEGIN
-    FOR v_registro_analise IN SELECT nome_pais FROM analise_vinhos LOOP
-        OPEN cur_descricao(p_pais := v_registro_analise.nome_pais);
+    FOR v_registro_analise IN SELECT DISTINCT country FROM provap1 WHERE country IS NOT NULL LOOP
+        OPEN cur_descricao(p_pais := v_registro_analise.country);
         FETCH cur_descricao INTO v_descricao_longa;
         CLOSE cur_descricao; 
         UPDATE analise_vinhos
         SET descricao_mais_longa = v_descricao_longa
-        WHERE nome_pais = v_registro_analise.nome_pais;
+        WHERE nome_pais = v_registro_analise.country;
+
+        -- RAISE NOTICE 'País: %, Descrição Encontrada: %', v_registro_analise.country, v_descricao_longa;
+
     END LOOP;
 END $$;
+
+-- DO $$
+-- DECLARE
+--     cur_descricao CURSOR (p_pais VARCHAR) FOR 
+--         SELECT description 
+--         FROM provap1
+--         WHERE country = p_pais 
+--         ORDER BY LENGTH(description) DESC 
+--         LIMIT 1;
+--     v_registro_analise RECORD;
+--     v_descricao_longa TEXT;
+-- BEGIN
+--     FOR v_registro_analise IN SELECT nome_pais FROM analise_vinhos LOOP
+--         OPEN cur_descricao(p_pais := v_registro_analise.nome_pais);
+--         FETCH cur_descricao INTO v_descricao_longa;
+--         CLOSE cur_descricao; 
+--         UPDATE analise_vinhos
+--         SET descricao_mais_longa = v_descricao_longa
+--         WHERE nome_pais = v_registro_analise.nome_pais;
+--     END LOOP;
+-- END $$;
 
 -- ----------------------------------------------------------------
 -- 4 Armazenamento dos resultados
